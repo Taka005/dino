365e29b4205c179f05dbae59db644fe9bc1e0292
Taka005 <01taka005@gmail.com>
2023-09-16 17:20:21 +0900

    fix

--- game.js
+++ game.js
@@ -1,7 +1,6 @@
 class Game{
   constructor(){
-    this.canvas = document.getElementById("game");
-    this.ctx = this.canvas.getContext("2d");
+    this.ctx = document.getElementById("game").getContext("2d");
   }
 
   /**
