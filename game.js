893a44e1964705c84725854dd82e956a8c97ed99
Taka005 <01taka005@gmail.com>
2023-09-16 23:11:05 +0900

    ミス

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 class Game{
   constructor(){
     this.canvas = document.getElementById("game");
-    this.ctx = this.canvas.getthis.ctx("2d");
+    this.ctx = this.canvas.getContext("2d");
   }
 
   /**
