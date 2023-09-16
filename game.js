a76b1ca8a8e1d2a77e3278fb61d9143f6c9732ef
Taka005 <01taka005@gmail.com>
2023-09-16 17:29:12 +0900

    fix

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ class Game{
     this.character = new Character("./img/avatar.png",30,30,30,250);
 
     this.loop = setInterval(()=>{
-      this.ctx.clearRect(0,0,canvas.width,canvas.height);
+      this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
       this.update();
       this.draw();
     },20);
