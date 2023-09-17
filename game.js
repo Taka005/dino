518d885008bae4cd70f81312e8595b0dc7cd0703
Taka005 <01taka005@gmail.com>
2023-09-17 23:50:36 +0900

    fix

--- game.js
+++ game.js
@@ -137,7 +137,8 @@ class Game{
       new Character("./img/enemy5.png",1000,400,64),
       new Character("./img/enemy6.png",1000,410,25),
       new Character("./img/enemy7.png",1000,400,60),
-      new Character("./img/enemy8.png",1000,425,40)
+      new Character("./img/enemy8.png",1000,430,35),
+      new Character("./img/enemy9.png",1000,400,60)
     ]);
 
     enemy.speedX = 15*Math.random()+10;
